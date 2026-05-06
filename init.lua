6d652536aff2ba15b23bd835c4845b5e64e37ea9
LegolandoBloom <72571763+LegolandoBloom@users.noreply.github.com>
2026-05-06 23:14:15 +0200

    added crosshair for abyss angling(not angled well) added abyss angler fishing spellID(doesn't work with Angleur because the object isn't soft-interactable)

--- init.lua
+++ init.lua
@@ -316,6 +316,8 @@ AngleurRetail_FishingSpellTable = {
     301092,
     -- Void Hole Fishing
     1224771,
+    -- Abyss Angler Fishing
+    1286703,
 }
 
 -- 1 : Retail | 2 : MoP(Or Cata) | 3 : Vanilla | (0: None, fail)
