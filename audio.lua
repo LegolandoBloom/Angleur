96b927eded798aad2dda4a40217b388c8eedf50b
LegolandoBloom <72571763+LegolandoBloom@users.noreply.github.com>
2026-06-17 11:57:13 +0300

    added separate method of getting the channel duration for Era and TBC(UnitChannelDuration() isn't defined)

--- audio.lua
+++ audio.lua
@@ -1,3 +1,7 @@
+-- 'ang' is the angleur namespace
+local addonName, ang = ...
+
+
 local debugChannel = 9
 local colorDebug = CreateColor(0.29, 1, 0) -- bright green
 
@@ -15,6 +19,22 @@ INFO_SPELLID_INDEX = 8
 -- audioHandlerFrame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
 -- audioHandlerFrame:SetScript("OnEvent", )
 
+local getChannelDuration
+if ang.gameVersion == 1 or ang.gameVersion == 2 then
+    getChannelDuration = function()
+        local durationObject = UnitChannelDuration("player")
+        local channelDuration = durationObject:GetTotalDuration()
+        return channelDuration
+    end
+elseif ang.gameVersion == 3 then
+    getChannelDuration = function()
+        local _, _, _, startTimeMs, endTimeMs = UnitChannelInfo("player")
+        local durationMs = endTimeMs - startTimeMs
+        local channelDuration = durationMs/1000
+        Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Classic channel duration: ", channelDuration)
+        return channelDuration
+    end
+end
 
 local reminderDelayer = CreateFrame("Frame")
 -- 0 -> inactive | 1 -> first part active(waiting until channel ends) | 2 ->  second part active(waiting an arbitrary amount until audio warning)
@@ -24,6 +44,7 @@ local function phase2()
     Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Phase 1 complete, starting phase 2")
     Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Phase 1 complete, starting phase 2")
     Angleur_SingleDelayer(PHASE2_DURATION, 0, DELAYER_THRESHOLD, reminderDelayer, nil, function()
+        scriptPhase = 0
         Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Phase 2 complete, playing sound")
         PlaySoundFile("Interface/AddOns/Angleur/sounds/angleurFailRecast.ogg")
     end)
@@ -51,8 +72,8 @@ function Angleur_RecastReminder_Start(spellID)
         scriptPhase = 0
         Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Phase 2 ended prematurely, removing script.")
     end
-    local durationObject = UnitChannelDuration("player")
-    local channelDuration = durationObject:GetTotalDuration()
+
+    local channelDuration = getChannelDuration()
     if not channelDuration then Angleur_BetaPrint(debugChannel, colorDebug:WrapTextInColorCode("Recast Reminder: ") .. "Channel duration nil. Returning.") return end
     channelDuration = channelDuration - PHASE1_SUBTRACTAMOUNT
     if channelDuration <= 0 then
