82bb9aeb9ac954ac0713cc34f07648df7cce4bd9
LegolandoBloom <72571763+LegolandoBloom@users.noreply.github.com>
2026-07-24 09:57:16 +0200

    added OnLoad method to SimpleArrowPopooutButton
    added SetState method as well to it
    
    now has an onUpdateFrame that will set updating state to false on the next update and remove its own script(To prevent SetState being called multiple times on the same update by OnHide of Popup + OnClick of the button)

--- angTemplates/LegolandoTemplates/Legolando_SimpleButtonTemplates/Legolando_SimpleButtonTemplates.lua
+++ angTemplates/LegolandoTemplates/Legolando_SimpleButtonTemplates/Legolando_SimpleButtonTemplates.lua
@@ -27,3 +27,35 @@ function Legolando_SimpleArrowPopoutButtonTemplateMixin_Angleur:SetRotate(facing
 	self.highlight:SetRotation(enum_Rotation[facing])
 	self.normal:SetRotation(enum_Rotation[facing])
 end
+
+function Legolando_SimpleArrowPopoutButtonTemplateMixin_Angleur:OnLoad()
+	self:GetNormalTexture():SetTexCoord(0.188, 0.828, 0.062, 0.437)
+	self:GetHighlightTexture():SetTexCoord(0.188, 0.828, 0.562, 0.937)
+	self.updating = false
+	self.onUpdateFrame = CreateFrame("Frame")
+	self.onUpdateFrame:Show()
+end
+
+function Legolando_SimpleArrowPopoutButtonTemplateMixin_Angleur:SetState(forceSet)
+	-- Only set state once per update
+	if self.updating == true then return end
+	local setTo = not self.expanded
+	if forceSet ~= nil then
+		setTo = forceSet
+	end
+	if setTo == false then
+		self:GetNormalTexture():SetTexCoord(0.188, 0.828, 0.062, 0.437)
+		self:GetHighlightTexture():SetTexCoord(0.188, 0.828, 0.562, 0.937)
+		self.expanded = false
+	elseif setTo == true then
+		self:GetNormalTexture():SetTexCoord(0.188, 0.828, 0.437, 0.062)
+		self:GetHighlightTexture():SetTexCoord(0.188, 0.828, 0.937, 0.562)
+		self.expanded = true
+	end
+	self.updating = true
+    self.onUpdateFrame:SetScript("OnUpdate", function(updaterFrame)
+        self.updating = false
+        updaterFrame:SetScript("OnUpdate", nil)
+    end)
+end
+
