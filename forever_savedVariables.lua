649d3b8c80141488661ad98ce5a3e19334848c8a
LegolandoBloom <72571763+LegolandoBloom@users.noreply.github.com>
2026-09-20 18:35:08 +0300

    added "no modifier" case for oneKey.
    Leave Keybind_Modifier empty if you don't want a modifier like so: Keybind_Modifier = ""
    
    :)

--- forever_savedVariables.lua
+++ forever_savedVariables.lua
@@ -14,13 +14,13 @@ local Minimap_Shown = ENABLED
 -- ____________________
 
 local Fishing_Method = "doubleClick" -- set to: "oneKey"  for keybind fishing
-local Keybind_Base = "SPACE" -- Can be set to any key OTHER than modifier keys: SHIFT, CTRL, ALT
-local Keybind_Modifier = "SHIFT" -- SHIFT, CTRL, ALT
+local Keybind_Base = "SPACE" -- Can be set to any key OTHER than modifier keys: "SHIFT", "CTRL", "ALT"
+local Keybind_Modifier = "SHIFT" -- "SHIFT", "CTRL", "ALT" | If you DON'T WANT a Modifier Key, leave empty: ""
 
 -- ______ Recast ______
 local Recast = DISABLED
 local Recast_Keybind = "SPACE" -- Can be set to any key OTHER than modifier keys: SHIFT, CTRL, ALT
-local Recast_Modifier = "CTRL" -- SHIFT, CTRL, ALT
+local Recast_Modifier = "CTRL" -- "SHIFT", "CTRL", "ALT" | If you DON'T WANT a Modifier Key, leave empty: ""
 -- ____________________
 
 -- ____ Ultra Focus ____
@@ -128,8 +128,32 @@ local WarningSound = ENABLED
 
 
 
+local angleurKey_fullKeybind
+local angleurKey_fullKeybind_base
+if not Keybind_Base or Keybind_Base == "" then
+    angleurKey_fullKeybind = "E"
+    angleurKey_fullKeybind_base = "E"
+elseif not Keybind_Modifier or Keybind_Modifier == "" then
+    angleurKey_fullKeybind = Keybind_Base
+    angleurKey_fullKeybind_base = angleurKey_fullKeybind
+else
+    angleurKey_fullKeybind = Keybind_Modifier .. "-" .. Keybind_Base
+    angleurKey_fullKeybind_base = Keybind_Base
+end
 
 
+local recastKey_fullKeybind
+local recastKey_fullKeybind_base
+if not Recast_Keybind or Recast_Keybind == "" then
+    recastKey_fullKeybind = "SHIFT-E"
+    recastKey_fullKeybind_base = "E"
+elseif not Recast_Modifier or Recast_Modifier == "" then
+    recastKey_fullKeybind = Recast_Keybind
+    recastKey_fullKeybind_base = recastKey_fullKeybind
+else
+    recastKey_fullKeybind = Recast_Modifier .. "-" .. Recast_Keybind
+    recastKey_fullKeybind_base = Recast_Keybind
+end
 
 
 
@@ -145,15 +169,15 @@ print(colorBlu:WrapTextInColorCode("Angleur Forever:") .. " Loaded.\nDue to a Bl
 
 
 AngleurConfig = {
-    angleurKey = Keybind_Modifier .. "-" .. Keybind_Base,
-    angleurKey_Base = Keybind_Base,
+    angleurKey = angleurKey_fullKeybind,
+    angleurKey_Base = angleurKey_fullKeybind_base,
     baitEnabled = nil,
     chosenBait = {itemID = 0, name = 0, dropDownID = 0},
     oversizedEnabled = nil,
     chosenMethod = Fishing_Method,
     doubleClickChosenID = 2,
     recastEnabled = Recast,
-    recastKey = Recast_Modifier .. "-" ..  Recast_Keybind,
+    recastKey = recastKey_fullKeybind,
     visualHidden = not Visual_Shown,
     visualLocation = {[1]="CENTER", [2]=nil, [3]="CENTER", [4]=Visual_XAxis, [5]=Visual_YAxis},
     ultraFocusAudioEnabled = UltraFocus_Audio,
